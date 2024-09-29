52df78fab02660f151e2d6a7dd6a46cea056234c
Jason Hoang <jvhoang@andrew.cmu.edu>
2024-09-29 17:50:22 -0400

    initial commit

---
+++ wifireceiver.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# begin_zero_padding = Number of zeros appended at the beginning of the message
+# in Level 4
+# message = The message used to create WiFi packet
+# length = the length of the message
+nfft = 64
+def WifiReceiver(output, level):
+    begin_zero_padding,message,length = 0
+    print(Interleave = np.reshape(np.transpose(np.reshape(np.arange(1, 2*nfft+1, 1),[-1,4])),[-1,]))
+    return begin_zero_padding, message, length
