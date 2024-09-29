5e1ff1c34f37fda15c32e239231efe1dbbf30a04
Jason Hoang <jvhoang@andrew.cmu.edu>
2024-09-29 18:37:50 -0400

    level1

--- wifireceiver.py
+++ wifireceiver.py
@@ -1,11 +1,61 @@
 import numpy as np
+import sys
+import commpy as comm
+from wifitransmitter import WifiTransmitter
+import commpy.channelcoding.convcode as check
 
-# begin_zero_padding = Number of zeros appended at the beginning of the message
-# in Level 4
-# message = The message used to create WiFi packet
-# length = the length of the message
-nfft = 64
 def WifiReceiver(output, level):
-    begin_zero_padding,message,length = 0
-    print(Interleave = np.reshape(np.transpose(np.reshape(np.arange(1, 2*nfft+1, 1),[-1,4])),[-1,]))
-    return begin_zero_padding, message, length
+    return 0,0,0
+
+import numpy as np
+
+def undo_interleave(output, Interleave, nfft):
+    nsym = int(len(output) / (2 * nfft))
+    bits = np.zeros_like(output)
+
+    for i in range(nsym):
+        symbol = output[i * 2 * nfft:(i + 1) * 2 * nfft]
+        bits[i * 2 * nfft:(i + 1) * 2 * nfft] = symbol[np.argsort(Interleave - 1)]
+    
+    return bits
+
+def test():
+    message = "hello"
+    level = 1
+    nfft = 64
+    Interleave = np.reshape(np.transpose(np.reshape(np.arange(1, 2 * nfft + 1, 1), [-1, 4])), [-1,])
+
+    if level >= 1:
+        bits = np.unpackbits(np.array([ord(c) for c in message], dtype=np.uint8))
+        print("Original bits before padding:")
+        print(bits)
+        
+        bits = np.pad(bits, (0, 2 * nfft - len(bits) % (2 * nfft)), 'constant')
+        nsym = int(len(bits) / (2 * nfft))
+        output = np.zeros(shape=(len(bits), ))
+
+        for i in range(nsym):
+            symbol = bits[i * 2 * nfft:(i + 1) * 2 * nfft]
+            output[i * 2 * nfft:(i + 1) * 2 * nfft] = symbol[Interleave - 1]
+
+    print("\nInterleaved bits:")
+    print(output)
+
+    original_bits = undo_interleave(output, Interleave, nfft)
+    print("\nBits after undoing interleaving:")
+    print(original_bits)
+
+    trimmed_bits = original_bits[:len(message) * 8].astype(np.uint8)
+    print("\nTrimmed bits (should match original bits):")
+    print(trimmed_bits)
+
+    byte_array = np.packbits(trimmed_bits).tobytes()
+    recovered_message = ''.join([chr(b) for b in byte_array])
+    print("\nRecovered message:")
+    print(recovered_message)
+
+test()
+
+
+
+
